5395665a93da3c8bbcb6b0241250c79b72f6f7af
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-06-08 23:10:53 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 Route::get("/", [JobController::class, 'index']);
+Route::get('/register', [RegisteredUsuerController::class, 'create']);
+Route::post('/register', [RegisteredUsuerController, 'store']);
+Route::get('/login', [SessionController, 'login'])->name('login');
+Route::post('/login', [SessionController, 'store']);
+Route::delete('/logout', [SessionController, 'logout']);

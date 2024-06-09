2df1f125950db8c7db82140c823db710640c6217
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-06-09 22:17:36 +0200

    update

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employer extends Model
 {
     use HasFactory;
-    public function User(): BelongsTo
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
     public function jobs()
     {
         return $this->hasMany(Job::class);
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\RegisteredUsuerController;
+use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
-Route::get('/register', [RegisteredUsuerController::class, 'create']);
-Route::post('/register', [RegisteredUsuerController::class, 'store']);
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::get('/login', [SessionController::class, 'login'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::delete('/logout', [SessionController::class, 'logout']);

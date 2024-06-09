21e7f46aa17933064c421f86feeb41b11b7612e3
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2024-06-09 18:40:58 +0200

    update

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,8 +1,1 @@
-@php
-    $classes = "bg-blue-800 rounded py-2 font-bold";
-@endphp
-
-
-<button {{ $attributes(['class'=> $classes]) }}>
-    {{ $slot }}
-</button>
+<button {{ $attributes(['class' => 'bg-blue-800 rounded py-2 px-6 font-bold']) }}>{{ $slot }}</button>
--- resources/views/components/section-heading.blade.php
+++ resources/views/components/section-heading.blade.php
@@ -1,8 +1,1 @@
-<?php
-$wrapper = 'inline-flex items-center gap-x-2';
-?>
-
-<div {{ $attributes(['class' => $wrapper]) }}>
-    <span class="inline-block h-2 w-2 bg-white"></span>
-    <h3 class="text-xl font-bold">{{ $slot }}</h3>
-</div>
+<h1 class="font-bold text-center text-4xl mb-8">{{ $slot }}</h1>
